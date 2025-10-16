a3834d3f6c08af87b780d97ce354cecbc5d7befc
ArturH16 <holandaartur163@gmail.com>
2025-10-16 11:42:26 -0300

    Estudo do objeto Number em JS

---
+++ guia-do-programador/praticas/objeto_number.js
@@ -0,0 +1,5 @@
+//Método toString transforma em string e pode receber um parâmetro que define qual base deverá ser feita a conversão
+let num = 2
+console.log(num.toString(2))
+let num2 = 19.5934896845
+console.log(num2.toFixed())
