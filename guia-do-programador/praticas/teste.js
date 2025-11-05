10b5d1c2cb59ffedbb7e1e617cc4788e648be462
ArturH16 <holandaartur163@gmail.com>
2025-11-05 20:32:01 -0300

    Estudos BOM

--- guia-do-programador/praticas/teste.js
+++ guia-do-programador/praticas/teste.js
@@ -1,21 +1,16 @@
-//Testes com Objetos
-let livro = new Object()
-livro.titulo = 'Ajax com jQuery'
-livro.autor = 'Mauricio Samy Silva'
-livro.paginas = 432
-livro.preco = 'R$ 69,00'
-livro.freteSedex = function(ceporigem, cepdestino, peso) {
-    let valorFrete = ''
-    //script do cálculo do frete
-    return valorFrete
+/*let temperatura = 30
+if (temperatura > 25) {
+    console.log('Está quente') }
+    else { console.log('Está frio')}*/
+
+function Pessoa(nome,idade) {
+    this.nome = nome
+    this.idade = idade
+     apresentar() {
+        console.log(`O meu nome é ${nome} e tenho ${anos} anos`)
+    }
 }
-livro.capitulo1 = 'Revisão do AJAX'
-livro.capitulo2 = 'Funções para requisições AJAX'
-livro.capitulo3 = 'Eventos e miscelânea'
-livro.capitulo4 = 'Requisições XML'
-livro.capitulo5 = 'Introdução ao formato JSON'
-livro.capitulo6 = 'Requisições JSON'
-let nomeAutor = livro.autor
-let capituloCinco = livro.capitulo5
-let valorFrete = livro.freteSedex(22050047,69190120,1060)
-console.log(`Autor: ${nomeAutor}\nCap 5: ${capituloCinco}\nValor frete: ${valorFrete}`)
+
+let artur = new Pessoa('artur',17)
+artur.apresentar()
+
